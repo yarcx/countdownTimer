e883415ad3192b7adac42758a07952126ff61687
yarcx <yakubuh56@gmail.com>
2023-10-24 20:58:50 +0100

    updated this shit

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,19 @@ const Completed = () => (
   </>
 );
 
-const renderer = (props: CountdownRenderProps) => {
-  const { hours, minutes, seconds, completed, days } = props;
-  if (completed) {
-    return <Completed />;
-  } else {
-    return (
-      <h1>
-        {days} days : {hours} hrs : {minutes} mins : {seconds} secs
-      </h1>
-    );
-  }
-};
-
 function App() {
+  const renderer = (props: CountdownRenderProps) => {
+    const { hours, minutes, seconds, completed, days } = props;
+    if (completed) {
+      return <Completed />;
+    } else {
+      return (
+        <h1>
+          {days} days : {hours} hrs : {minutes} mins : {seconds} secs
+        </h1>
+      );
+    }
+  };
   return (
     <>
       <Countdown date={new Date("2024-1-1")} renderer={renderer} />
